919d8f51f221205419173383722c2aabfe9f55d5
genilson <geniilsonfernandes@gmail.com>
2022-10-24 14:15:33 -0300

    :hammer:: configure eslint

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/styles/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>
+          \jsx-eslint\eslint-plugin-react\tree\master\docs\rules\react-in-jsx-scope.md
+          Open up App.tsx to dsfsfsfsfsdfdsgdf gdg fd hfh gfstart working on
+          your app!
+        </Text>
+
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
