07f4bc7ab5ace6ae8311706c7f043fd3c0f4edea
genilson <geniilsonfernandes@gmail.com>
2022-10-24 19:58:57 -0300

    style: configure theme

--- App.tsx
+++ App.tsx
@@ -1,29 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold
+} from "@expo-google-fonts/inter";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import { ThemeProvider } from "styled-components";
+
 import theme from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <View style={styles.container}>
-        <Text>
-          \jsx-eslint\eslint-plugin-react\tree\master\docs\rules\react-in-jsx-scope.md
-          Open up App.tsx to dsfsfsfsfsdfdsgdf gdg fd hfh gfstart working on
-          your app!
-        </Text>
-
-        <StatusBar style="auto" />
-      </View>
+      <StatusBar style="auto" />
+      {fontsLoaded ? (
+        <View>
+          <Text>
+            \jsx-eslint\eslint-plugin-react\tree\master\docs\rules\react-in-jsx-scope.md
+            Open up App.tsx to dsfsfsfsfsdfdsgdf gdg fd hfh gfstart working on
+            your app!
+          </Text>
+        </View>
+      ) : (
+        <ActivityIndicator />
+      )}
     </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
