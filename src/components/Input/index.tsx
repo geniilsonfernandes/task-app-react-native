c02aa4daf78f4a1137447e7a23ea7886a478d4bf
genilson <geniilsonfernandes@gmail.com>
2022-10-25 19:33:25 -0300

    refactor: add callback functions

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
 
 import * as S from "./styles";
 import { useTheme } from "styled-components/native";
+import { Alert } from "react-native";
 
-export const Input = () => {
+type InputProps = {
+  onSubmit: (task: string) => string;
+};
+
+export const Input = ({ onSubmit }: InputProps) => {
   const theme = useTheme();
+  const [value, setValue] = useState("");
+
+  const handleSubmitValue = () => {
+    if (value.trim().length > 0) {
+      onSubmit && onSubmit(value);
+    } else {
+      Alert.alert("Nova Tarefa", "digite sua tarefa");
+    }
+  };
+
   return (
     <S.Wrapper>
       <S.Input
         placeholder="Adicione uma nova tarefa"
         placeholderTextColor={theme.COLORS.GRAY_300}
+        onChangeText={setValue}
+        multiline={true}
       />
-      <S.Button>
+      <S.Button onPress={() => handleSubmitValue()}>
         <AntDesign name="pluscircleo" size={18} color="white" />
       </S.Button>
     </S.Wrapper>
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -8,12 +8,14 @@ export const Wrapper = styled.View`
 export const Input = styled(TextInput)`
   ${({ theme }) => css`
     padding: 16px;
-    height: 54px;
+    min-height: 54px;
+    padding: 10px;
     flex: 1;
     border-radius: 6px;
     background: ${theme.COLORS.GRAY_500};
     border: 1px solid ${theme.COLORS.GRAY_700};
     margin-right: 4px;
+    color: ${theme.COLORS.GRAY_100};
   `}
 `;
 export const Button = styled.TouchableOpacity`
