1386463b5a727f4f21dfdb80fb7fda9e0a08a610
genilson <geniilsonfernandes@gmail.com>
2022-10-25 08:51:45 -0300

    config types

--- App.tsx
+++ App.tsx
@@ -1,31 +1,23 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   useFonts,
   Inter_400Regular,
   Inter_700Bold
 } from "@expo-google-fonts/inter";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
 
 import theme from "./src/styles/theme";
+import Todo from "./src/screens/Todo";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="auto" />
-      {fontsLoaded ? (
-        <View>
-          <Text>
-            \jsx-eslint\eslint-plugin-react\tree\master\docs\rules\react-in-jsx-scope.md
-            Open up App.tsx to dsfsfsfsfsdfdsgdf gdg fd hfh gfstart working on
-            your app!
-          </Text>
-        </View>
-      ) : (
-        <ActivityIndicator />
-      )}
+      <StatusBar style="light" />
+      {fontsLoaded ? <Todo /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
