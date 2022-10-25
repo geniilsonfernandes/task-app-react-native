74562e985fc442e9fd0416d545e20c2137ec14c4
genilson <geniilsonfernandes@gmail.com>
2022-10-25 20:11:31 -0300

    refactor: remove styles dont used

--- src/components/Head/styles.ts
+++ src/components/Head/styles.ts
@@ -3,6 +3,4 @@ import styled from "styled-components/native";
 export const Wrapper = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  padding: 0 24px;
-  padding-top: 48px;
 `;
