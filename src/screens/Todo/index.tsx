77b6a966d97dc5ed359e83340266a96e4bd133a2
genilson <geniilsonfernandes@gmail.com>
2022-10-25 20:13:37 -0300

    create screen todo

--- src/screens/Todo/index.tsx
+++ src/screens/Todo/index.tsx
@@ -1,20 +1,56 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
+import { Counter } from "../../components/Counter";
 import { Head } from "../../components/Head";
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
 import { Task } from "../../components/Task";
 
 import * as S from "./styles";
 
+const data = [
+  { task: "task 1", create_at: "12,12,12", check: false, id: "123" }
+];
+
 const Todo = () => {
+  const checkedTasks = data.filter((task) => task.check).length;
+  const createdTasks = data.length;
+
+  const handleAddNewTask = (task: string) => {
+    console.log(task);
+  };
+
+  const handleRemoveTask = (id: string) => {
+    console.log(id, "handleRemoveTask");
+  };
+  const handleCheckTask = (status: boolean) => {
+    console.log(status);
+  };
+
   return (
     <S.Wrapper>
       <Header />
-      <Head />
-      <S.TaskList>
-        <Task />
-        <Task />
-      </S.TaskList>
+      <S.TodoWrapper>
+        <S.InputWrapper>
+          <Input onSubmit={(task) => handleAddNewTask(task)} />
+        </S.InputWrapper>
+        <S.HeadWrapper>
+          <Counter title="Criadas" color="blue" counter={createdTasks} />
+          <Counter title="Concluídas" color="purple" counter={checkedTasks} />
+        </S.HeadWrapper>
+        <FlatList
+          data={data}
+          keyExtractor={({ id }) => id}
+          renderItem={({ item }) => (
+            <Task
+              task={item.task}
+              check={item.check}
+              onRemove={() => handleRemoveTask(item.id)}
+              onCheck={(check) => handleCheckTask(check)}
+            />
+          )}
+        />
+      </S.TodoWrapper>
     </S.Wrapper>
   );
 };
--- src/screens/Todo/styles.ts
+++ src/screens/Todo/styles.ts
@@ -1,12 +1,19 @@
 import styled, { css } from "styled-components/native";
 
-export const Wrapper = styled.ScrollView`
+export const Wrapper = styled.View`
   ${({ theme }) => css`
     background: ${theme.COLORS.GRAY_600};
     flex: 1;
   `}
 `;
-export const TaskList = styled.View`
+export const TodoWrapper = styled.View`
   padding: 0 24px;
-  padding-top: 24px;
+`;
+export const InputWrapper = styled.View`
+  margin-top: -27px;
+`;
+export const HeadWrapper = styled.View`
+  padding: 24px 0;
+  flex-direction: row;
+  justify-content: space-between;
 `;
